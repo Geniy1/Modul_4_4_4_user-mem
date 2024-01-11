4872f925577ff21bf43161ef16208119cc18e4d2
alfatech.uz <golibjonodinaev@gmail.com>
2024-01-11 17:29:04 +0500

    Restart MainActivaty is correct

--- app/src/main/java/com/example/user_member/MainActivity.java
+++ app/src/main/java/com/example/user_member/MainActivity.java
@@ -9,11 +9,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    public User user;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        // Intent orqali User obyekti olish
+//        Intent intent = getIntent();
+//        Member member = (Member) intent.getSerializableExtra("memberObject");
+//        // TextView ga ma'lumotni joylash
+//        TextView infoTV = findViewById(R.id.textV);
+//        String memInfo = "Member: "+member.getUsername()+ ", e-mail: "+member.getEmail();
+//        infoTV.setText(memInfo);
+
+        user = new User("Odinayev", 43);
+        Button okB = findViewById(R.id.okButton);
+        okB.setOnClickListener(view -> {
+            // Intent orqali SecondActivityga o'tish
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            intent.putExtra("userObject", user);
+            startActivity(intent);
+        });
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
         setContentView(R.layout.activity_main);
         // Intent orqali User obyekti olish
         Intent intent = getIntent();
@@ -21,15 +42,6 @@ public class MainActivity extends AppCompatActivity {
         // TextView ga ma'lumotni joylash
         TextView infoTV = findViewById(R.id.textV);
         String memInfo = "Member: "+member.getUsername()+ ", e-mail: "+member.getEmail();
-        infoTV.setText(memInfo);
-
-        user = new User("Odinayev", 43);
-        Button okButton = findViewById(R.id.okButton);
-        okButton.setOnClickListener(view -> {
-            // Intent orqali SecondActivityga o'tish
-            Intent intent1 = new Intent(MainActivity.this, SecondActivity.class);
-            intent1.putExtra("userObject", user);
-            startActivity(intent1);
-        });
+        infoTV.setText(memInfo);;
     }
 }
